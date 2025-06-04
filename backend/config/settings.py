369bc5ae32877c1e2012951ddbdbda4445eb67c7
Denis <zemdenalex@gmail.com>
2025-06-04 18:28:58 +0300

    cors fix

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -155,7 +155,9 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:8000",
     "http://127.0.0.1:8000",
     "http://localhost:3000",
-    "http://127.0.0.1:3000"
+    "http://127.0.0.1:3000",
+    "http://127.0.0.1:5500",
+    "http://localhost:5500",
 ]
 CORS_ALLOW_CREDENTIALS = True
 
